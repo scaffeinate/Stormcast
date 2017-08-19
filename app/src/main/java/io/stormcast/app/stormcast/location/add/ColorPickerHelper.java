b6341c82a8273bee4747d25d46e4890fa3942948
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-08-18 22:04:02 -0400

    Move color picker functionality into helper class

--- app/src/main/java/io/stormcast/app/stormcast/location/add/ColorPickerHelper.java
+++ app/src/main/java/io/stormcast/app/stormcast/location/add/ColorPickerHelper.java
@@ -1,8 +1,47 @@
 package io.stormcast.app.stormcast.location.add;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v4.content.ContextCompat;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+
 /**
  * Created by sudharti on 8/18/17.
  */
 
 public class ColorPickerHelper {
+
+     interface ColorPickerCallback {
+        void onColorSelected(int color);
+    }
+
+    protected static void showColorPicker(Context context, int defaultColor, final ColorPickerCallback callback) {
+        ColorPickerDialogBuilder.with(context)
+                .setTitle("Choose color")
+                .initialColor(ContextCompat.getColor(context, defaultColor))
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int color) {
+                        callback.onColorSelected(color);
+                    }
+                })
+                .setPositiveButton("Ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
+    }
 }
