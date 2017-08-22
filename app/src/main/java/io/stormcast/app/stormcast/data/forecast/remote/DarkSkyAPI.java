fe675f7934917304dfbefded9761500a99d7be9a
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-08-22 03:11:25 -0400

    Add retrofit interface and a class for the actual call

---
+++ app/src/main/java/io/stormcast/app/stormcast/data/forecast/remote/DarkSkyAPI.java
@@ -0,0 +1,15 @@
+package io.stormcast.app.stormcast.data.forecast.remote;
+
+import io.stormcast.app.stormcast.common.models.Forecast;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by sudharti on 8/22/17.
+ */
+
+public interface DarkSkyAPI {
+    @GET("forecast/{apiKey}/{latLng}")
+    Call<Forecast> getForecast(@Path("apiKey") String apiKey, @Path("latitude") String latLng);
+}
---
+++ app/src/main/java/io/stormcast/app/stormcast/data/forecast/remote/DarkSkyService.java
@@ -0,0 +1,8 @@
+package io.stormcast.app.stormcast.data.forecast.remote;
+
+/**
+ * Created by sudharti on 8/22/17.
+ */
+
+public class DarkSkyService {
+}
