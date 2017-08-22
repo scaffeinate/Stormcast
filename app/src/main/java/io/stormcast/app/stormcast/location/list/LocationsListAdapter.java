7af0fbfb3347e65c025a63bb13afc168fb72ff67
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-08-22 00:00:12 -0400

    Changes to fragments

--- app/src/main/java/io/stormcast/app/stormcast/location/list/LocationsListAdapter.java
+++ app/src/main/java/io/stormcast/app/stormcast/location/list/LocationsListAdapter.java
@@ -36,10 +36,6 @@ public class LocationsListAdapter extends RecyclerView.Adapter<LocationsListAdap
         Location location = mLocationList.get(position);
         if (location != null) {
             holder.nameTextView.setText(location.getName());
-            GradientDrawable background = (GradientDrawable) holder.bgColorImageView.getBackground();
-            background.setColor(Color.parseColor(location.getBackgroundColor()));
-            background = (GradientDrawable) holder.textColorImageView.getBackground();
-            background.setColor(Color.parseColor(location.getTextColor()));
         }
     }
 
@@ -51,15 +47,12 @@ public class LocationsListAdapter extends RecyclerView.Adapter<LocationsListAdap
     static class ViewHolder extends RecyclerView.ViewHolder {
         private final View parentView;
         private final TextView nameTextView;
-        private final ImageView bgColorImageView, textColorImageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             this.parentView = itemView;
 
             nameTextView = (TextView) itemView.findViewById(R.id.location_name_text_view);
-            bgColorImageView = (ImageView) itemView.findViewById(R.id.bg_color_image_view);
-            textColorImageView = (ImageView) itemView.findViewById(R.id.text_color_image_view);
         }
     }
 }
--- app/src/main/java/io/stormcast/app/stormcast/weather/WeatherFragment.java
+++ app/src/main/java/io/stormcast/app/stormcast/weather/WeatherFragment.java
@@ -1,12 +1,17 @@
 package io.stormcast.app.stormcast.weather;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import io.stormcast.app.stormcast.R;
 import io.stormcast.app.stormcast.common.Location;
@@ -19,6 +24,8 @@ public class WeatherFragment extends Fragment {
 
     private static final String LOCATION = "LOCATION";
     private RecyclerView mRecyclerView;
+    private ActionBar mActionBar;
+    private Location mLocation;
 
     public static WeatherFragment newInstance(Location location) {
         WeatherFragment weatherFragment = new WeatherFragment();
@@ -30,10 +37,23 @@ public class WeatherFragment extends Fragment {
         return weatherFragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLocation = getArguments().getParcelable(LOCATION);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mRecyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_location_detail, container, false);
-        return mRecyclerView;
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.weather_info_recycler_view);
+        view.setBackgroundColor(Color.parseColor(mLocation.getBackgroundColor()));
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
