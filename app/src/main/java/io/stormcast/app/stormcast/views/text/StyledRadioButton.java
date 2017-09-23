61e1c330c2b4fda1112cf5010079c0fafb23651a
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-09-23 02:03:00 -0400

    Add styled radio button view component

---
+++ app/src/main/java/io/stormcast/app/stormcast/views/text/StyledRadioButton.java
@@ -0,0 +1,28 @@
+package io.stormcast.app.stormcast.views.text;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+
+import io.stormcast.app.stormcast.R;
+
+/**
+ * Created by sudharti on 9/23/17.
+ */
+
+public class StyledRadioButton extends android.support.v7.widget.AppCompatRadioButton {
+    public StyledRadioButton(Context context) {
+        this(context, null);
+    }
+
+    public StyledRadioButton(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.radioButtonStyle);
+    }
+
+    public StyledRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.StyledRadioButton);
+        String fontWeight = arr.getString(R.styleable.StyledRadioButton_rb_weight);
+        setTypeface(TypeFaceUtil.getTypeFace(context, fontWeight));
+    }
+}
