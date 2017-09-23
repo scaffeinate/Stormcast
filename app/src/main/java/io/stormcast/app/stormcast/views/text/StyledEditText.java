60a8354671bd11dcde326368833457677711cfd0
Sudharsanan <sudhar.ti@gmail.com>
2017-09-22 23:38:03 -0400

    Add styled text views

---
+++ app/src/main/java/io/stormcast/app/stormcast/views/text/StyledEditText.java
@@ -0,0 +1,8 @@
+package io.stormcast.app.stormcast.views.text;
+
+/**
+ * Created by sudhar on 9/22/17.
+ */
+
+public class StyledEditText {
+}
---
+++ app/src/main/java/io/stormcast/app/stormcast/views/text/StyledTextView.java
@@ -0,0 +1,30 @@
+package io.stormcast.app.stormcast.views.colorpick;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by sudhar on 9/22/17.
+ */
+
+public class StyledTextView extends android.support.v7.widget.AppCompatTextView {
+    public StyledTextView(Context context) {
+        this(context, null);
+    }
+
+    public StyledTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.textViewStyle);
+    }
+
+    public StyledTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.setFont();
+    }
+
+    private void setFont() {
+        Typeface font = Typeface.createFromAsset(getContext().getAssets(), "font/futura_pt_book.otf");
+        setTypeface(font);
+    }
+}
