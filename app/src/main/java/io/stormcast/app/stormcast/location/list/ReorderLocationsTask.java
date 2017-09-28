3cbc196bb5cee4bd5eea8d0a3d11a6c7ad64b800
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-09-28 02:00:36 -0400

    Do the DB update in an AsyncTask

---
+++ app/src/main/java/io/stormcast/app/stormcast/location/list/ReorderLocationsTask.java
@@ -0,0 +1,43 @@
+package io.stormcast.app.stormcast.location.list;
+
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import io.stormcast.app.stormcast.common.models.LocationModel;
+
+/**
+ * Created by sudharti on 9/28/17.
+ */
+
+public class ReorderLocationsTask extends AsyncTask<Void, Integer, Void> {
+
+    private List<LocationModel> mLocationModelList;
+    private int fromPosition = 0, toPosition = 0;
+    private LocationsListContract.Presenter mPresenter;
+
+    public ReorderLocationsTask(LocationsListContract.Presenter presenter, List<LocationModel> locationModelList,
+                                int fromPosition, int toPosition) {
+        this.mPresenter = presenter;
+        this.mLocationModelList = locationModelList;
+        this.fromPosition = fromPosition;
+        this.toPosition = toPosition;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        if (fromPosition < toPosition) {
+            for (int i = fromPosition; i <= toPosition; i++) {
+                LocationModel locationModel = mLocationModelList.get(i);
+                locationModel.setPosition(i);
+            }
+        } else {
+            for (int i = toPosition; i <= fromPosition; i++) {
+                LocationModel locationModel = mLocationModelList.get(i);
+                locationModel.setPosition(i);
+            }
+        }
+        mPresenter.reorder(mLocationModelList);
+        return null;
+    }
+}
