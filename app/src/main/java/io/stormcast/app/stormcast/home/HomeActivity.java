3a6fc97eb0fe10f7216eec14203d2d73cffc9031
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-09-30 23:41:24 -0400

    Use NavDrawer in MainActivity

--- app/src/main/java/io/stormcast/app/stormcast/home/HomeActivity.java
+++ app/src/main/java/io/stormcast/app/stormcast/home/HomeActivity.java
@@ -2,24 +2,43 @@ package io.stormcast.app.stormcast.home;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import io.stormcast.app.stormcast.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavDrawerFragment.NavDrawerCallbacks {
 
-    private ActionBar mActionBar;
     private FragmentManager mFragmentManager;
     private HomeFragment mHomeFragment;
+    private DrawerLayout mDrawerLayout;
+    private NavDrawerFragment mNavDrawerFragment;
+    private ActionBar mActionBar;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mActionBar = getSupportActionBar();
         mFragmentManager = getSupportFragmentManager();
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavDrawerFragment = (NavDrawerFragment) mFragmentManager.findFragmentById(R.id.fragment_nav_drawer);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mActionBar = getSupportActionBar();
+
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mActionBar.setHomeButtonEnabled(true);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        mActionBar.setElevation(0);
+
+        mNavDrawerFragment.setUp(R.id.fragment_nav_drawer, mDrawerLayout);
 
         if (savedInstanceState == null) {
             mHomeFragment = HomeFragment.newInstance();
@@ -28,7 +47,10 @@ public class HomeActivity extends AppCompatActivity {
                     .replace(R.id.main_content, mHomeFragment)
                     .commit();
         }
+    }
 
-        if (mActionBar != null) mActionBar.setElevation(0);
+    @Override
+    public void onNavDrawerListItemClicked(int position) {
+
     }
 }
