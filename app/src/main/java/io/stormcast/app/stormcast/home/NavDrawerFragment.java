90fd92de226512ca2967db675592df0942088625
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-09-30 23:40:44 -0400

    Add NavDrawer

---
+++ app/src/main/java/io/stormcast/app/stormcast/home/NavDrawerFragment.java
@@ -0,0 +1,131 @@
+package io.stormcast.app.stormcast.home;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import io.stormcast.app.stormcast.R;
+
+/**
+ * Created by sudharti on 9/30/17.
+ */
+
+public class NavDrawerFragment extends Fragment {
+    private View mDrawerFragment;
+    private ListView mNavDrawerListView;
+    private DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mActionBarToggle;
+
+    private String[] mNavDrawerArray;
+    private ArrayAdapter<String> mNavDrawerAdapter;
+    private NavDrawerCallbacks mNavDrawerCallbacks;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mNavDrawerCallbacks = (NavDrawerCallbacks) getActivity();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNavDrawerArray = new String[]{};
+        mNavDrawerAdapter = new ArrayAdapter<>(getContext(), R.layout.item_nav_drawer_list, mNavDrawerArray);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mNavDrawerListView = (ListView) inflater.inflate(R.layout.fragment_nav_drawer, container, false);
+        mNavDrawerListView.setAdapter(mNavDrawerAdapter);
+        mNavDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectItem(i);
+            }
+        });
+        return mNavDrawerListView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mNavDrawerCallbacks = null;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mActionBarToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setUp(int navDrawerFragmentId, DrawerLayout drawerLayout) {
+        this.mDrawerLayout = drawerLayout;
+        this.mDrawerFragment = getActivity().findViewById(navDrawerFragmentId);
+
+        mActionBarToggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout,
+                R.string.nav_drawer_open, R.string.nav_drawer_close);
+        mActionBarToggle.syncState();
+        mDrawerLayout.addDrawerListener(mActionBarToggle);
+
+        selectItem(0);
+    }
+
+    private void selectItem(final int position) {
+        if (mNavDrawerCallbacks != null) {
+            if (isDrawerOpen()) closeDrawer();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mNavDrawerCallbacks.onNavDrawerListItemClicked(position);
+                }
+            }, 200);
+            mNavDrawerListView.setItemChecked(position, true);
+        }
+    }
+
+    private boolean isDrawerOpen() {
+        return mDrawerLayout.isDrawerOpen(mDrawerFragment);
+    }
+
+    private void closeDrawer() {
+        mDrawerLayout.closeDrawer(mDrawerFragment);
+    }
+
+    public void toggleMenu(boolean enabled) {
+        if (enabled) {
+            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNDEFINED);
+            //mActionBarToggle.animateToMenu();
+        } else {
+            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            //mActionBarToggle.animateToBackArrow();
+        }
+    }
+
+    public interface NavDrawerCallbacks {
+        public void onNavDrawerListItemClicked(int position);
+    }
+}
