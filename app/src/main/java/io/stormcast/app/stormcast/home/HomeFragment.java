9510e659ee283413b915b5564e083c5e18b3bf7d
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-09-30 23:41:48 -0400

    Fix the toolbar

--- app/src/main/java/io/stormcast/app/stormcast/home/HomeFragment.java
+++ app/src/main/java/io/stormcast/app/stormcast/home/HomeFragment.java
@@ -1,17 +1,15 @@
 package io.stormcast.app.stormcast.home;
 
 import android.content.Context;
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.github.pwittchen.weathericonview.WeatherIconView;
 
 import java.util.List;
 
@@ -19,23 +17,25 @@ import io.stormcast.app.stormcast.R;
 import io.stormcast.app.stormcast.common.models.LocationModel;
 import io.stormcast.app.stormcast.data.locations.LocationsRepository;
 import io.stormcast.app.stormcast.data.locations.local.LocalLocationsDataSource;
-import io.stormcast.app.stormcast.location.LocationsActivity;
+import io.stormcast.app.stormcast.views.styled.StyledTextView;
 
 /**
  * Created by sudhar on 8/8/17.
  */
 
-public class HomeFragment extends Fragment implements HomeContract.View, View.OnClickListener {
+public class HomeFragment extends Fragment implements HomeContract.View, ViewPager.OnPageChangeListener {
 
     private Context mContext;
 
     private ViewPager mViewPager;
-    private WeatherIconView mSplashIcon;
-    private FloatingActionButton mButton;
 
     private HomeViewPagerAdapter mViewPagerAdapter;
     private HomePresenter mHomePresenter;
     private LocationsRepository mLocationsRepository;
+    private List<LocationModel> mLocationModels;
+
+    private Toolbar mToolbar;
+    private StyledTextView mToolbarTitle;
 
     public static HomeFragment newInstance() {
         HomeFragment mFragment = new HomeFragment();
@@ -56,9 +56,8 @@ public class HomeFragment extends Fragment implements HomeContract.View, View.On
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View mView = inflater.inflate(R.layout.fragment_home, container, false);
         mViewPager = (ViewPager) mView.findViewById(R.id.view_pager);
-        mSplashIcon = (WeatherIconView) mView.findViewById(R.id.splash_storm_icon);
-        mButton = (FloatingActionButton) mView.findViewById(R.id.locations_button);
-        mButton.setOnClickListener(this);
+        mToolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        mToolbarTitle = (StyledTextView) getActivity().findViewById(R.id.toolbar_title);
         return mView;
     }
 
@@ -82,21 +81,32 @@ public class HomeFragment extends Fragment implements HomeContract.View, View.On
             mViewPagerAdapter.setLocations(locationModels);
             mViewPagerAdapter.notifyDataSetChanged();
         }
+        this.mLocationModels = locationModels;
+        setTitle(locationModels.get(0));
+        mViewPager.addOnPageChangeListener(this);
     }
 
     @Override
     public void onDataNotAvailable() {
         mViewPager.setVisibility(View.GONE);
-        mSplashIcon.setVisibility(View.VISIBLE);
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.locations_button:
-                Intent mIntent = new Intent(getActivity(), LocationsActivity.class);
-                startActivity(mIntent);
-                break;
-        }
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        setTitle(mLocationModels.get(position));
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    private void setTitle(LocationModel locationModel) {
+        mToolbarTitle.setText(locationModel.getName());
     }
 }
